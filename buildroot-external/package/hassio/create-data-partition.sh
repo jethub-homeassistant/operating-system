cd0a1e1cd49c2d0ad6252339dc122c0051ea5118
Viacheslav Bocharov <adeep@lexina.in>
2024-06-18 12:02:37 +0300

    Add JetHub H1/D1+/D2 configs

--- buildroot-external/package/hassio/create-data-partition.sh
+++ buildroot-external/package/hassio/create-data-partition.sh
@@ -8,7 +8,7 @@ data_img="${dst_dir}/data.ext4"
 
 # Make image
 rm -f "${data_img}"
-truncate --size="1280M" "${data_img}"
+truncate --size="6000M" "${data_img}"
 mkfs.ext4 -L "hassos-data" -E lazy_itable_init=0,lazy_journal_init=0 "${data_img}"
 
 # Mount / init file structs
@@ -27,6 +27,15 @@ container=$(docker run --privileged -e DOCKER_TLS_CERTDIR="" \
 docker exec "${container}" sh /build/dind-import-containers.sh
 
 docker stop "${container}"
-
+sleep 10
+sudo sync
+sleep 10
 # Unmount data image
 sudo umount "${build_dir}/data/"
+
+sudo sync
+
+e2fsck -f -p "${data_img}"
+resize2fs "${data_img}"
+
+
