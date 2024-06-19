76548f69080a29493184c3b1a1edf8eb2bb1a309
Viacheslav Bocharov <adeep@lexina.in>
2024-06-19 16:19:23 +0300

    Update board configs

--- buildroot-external/board/jethome/jethub-j200/hassos-hook.sh
+++ buildroot-external/board/jethome/jethub-j200/hassos-hook.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # shellcheck disable=SC2155
 
+# shellcheck source=../../../scripts/burn.sh
+. "${SCRIPT_DIR}/burn.sh"
+
 function hassos_pre_image() {
     local BOOT_DATA="$(path_boot_dir)"
-#    local UBOOT="${BINARIES_DIR}/u-boot.bin.sd.bin"
-#    local SPL_IMG="$(path_spl_img)"
 
     cp "${BINARIES_DIR}/boot.scr" "${BOOT_DATA}/boot.scr"
     mkdir -p "${BOOT_DATA}/amlogic"
@@ -17,15 +18,11 @@ function hassos_pre_image() {
     fi
     cp "${BOARD_DIR}/boot-env.txt" "${BOOT_DATA}/hassos-config.txt" || true
     cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
-
-#    # SPL
-#    create_spl_image
-#    dd if="${UBOOT}" of="${SPL_IMG}" bs=1 count=442 conv=fsync
-#    dd if="${UBOOT}" of="${SPL_IMG}" bs=512 skip=1 seek=1 conv=fsync
 }
 
 function hassos_post_image() {
     convert_disk_image_xz
     # support for create AmLogic burnable images
+    [[ -f "${BINARIES_DIR}/platform.conf" ]] && _create_disk_burn
     [[ -f "${BINARIES_DIR}/platform.conf" ]] && convert_disk_image_burn_zip
 }
