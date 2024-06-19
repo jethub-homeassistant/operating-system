d3b4fb1251ee51caff25302bbb4eb7cc17ff88bb
Viacheslav Bocharov <adeep@lexina.in>
2024-06-19 16:19:23 +0300

    Update board configs

--- buildroot-external/board/jethome/jethub-j80/hassos-hook.sh
+++ buildroot-external/board/jethome/jethub-j80/hassos-hook.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # shellcheck disable=SC2155
 
-#    local UBOOT="${BINARIES_DIR}/u-boot.bin.sd.bin"
-#    local SPL_IMG="$(path_spl_img)"
-#    # SPL
-#    create_spl_image
-#    dd if="${UBOOT}" of="${SPL_IMG}" bs=1 count=442 conv=fsync
-#    dd if="${UBOOT}" of="${SPL_IMG}" bs=512 skip=1 seek=1 conv=fsync
+# shellcheck source=../../../scripts/burn.sh
+. "${SCRIPT_DIR}/burn.sh"
 
 function hassos_pre_image() {
     local BOOT_DATA="$(path_boot_dir)"
@@ -27,5 +23,7 @@ function hassos_pre_image() {
 
 function hassos_post_image() {
     convert_disk_image_xz
+    # support for create AmLogic burnable images
+    [[ -f "${BINARIES_DIR}/platform.conf" ]] && _create_disk_burn
     [[ -f "${BINARIES_DIR}/platform.conf" ]] && convert_disk_image_burn_zip
 }
