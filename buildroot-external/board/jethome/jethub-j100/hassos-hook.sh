f1515847ca945dff01fa1b7691c111ef61fd4a17
Viacheslav Bocharov <adeep@lexina.in>
2024-06-19 15:06:23 +0300

    Update boad configs (add meta/image-hook/spl configs)

--- buildroot-external/board/jethome/jethub-j100/hassos-hook.sh
+++ buildroot-external/board/jethome/jethub-j100/hassos-hook.sh
@@ -3,8 +3,8 @@
 
 function hassos_pre_image() {
     local BOOT_DATA="$(path_boot_dir)"
-    local UBOOT="${BINARIES_DIR}/u-boot.bin.sd.bin"
-    local SPL_IMG="$(path_spl_img)"
+#    local UBOOT="${BINARIES_DIR}/u-boot.bin.sd.bin"
+#    local SPL_IMG="$(path_spl_img)"
 
     cp "${BINARIES_DIR}/boot.scr" "${BOOT_DATA}/boot.scr"
     mkdir -p "${BOOT_DATA}/amlogic"
@@ -12,13 +12,18 @@ function hassos_pre_image() {
     cp "${BINARIES_DIR}/meson-axg-jethome-jethub-j110-rev-2.dtb" "${BOOT_DATA}/amlogic/"
     cp "${BINARIES_DIR}/meson-axg-jethome-jethub-j110-rev-3.dtb" "${BOOT_DATA}/amlogic/"
 
+    if ls "${BINARIES_DIR}"/*.dtbo 1> /dev/null 2>&1; then
+        echo "Found .dtbo files in ${BINARIES_DIR}"
+        mkdir -p "${BOOT_DATA}/overlays"
+        cp "${BINARIES_DIR}"/*.dtbo "${BOOT_DATA}/overlays/"
+    fi
     cp "${BOARD_DIR}/boot-env.txt" "${BOOT_DATA}/hassos-config.txt" || true
     cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
 
-    # SPL
-    create_spl_image
-    dd if="${UBOOT}" of="${SPL_IMG}" bs=1 count=442 conv=fsync
-    dd if="${UBOOT}" of="${SPL_IMG}" bs=512 skip=1 seek=1 conv=fsync
+#    # SPL
+#    create_spl_image
+#    dd if="${UBOOT}" of="${SPL_IMG}" bs=1 count=442 conv=fsync
+#    dd if="${UBOOT}" of="${SPL_IMG}" bs=512 skip=1 seek=1 conv=fsync
 }
 
 function hassos_post_image() {
